9a0411b17c321a1b316babe791ecb072b6d9a0a8
yungang <fyg1987630@gmail.com>
2017-09-04 22:32:21 +1000

    add a test picture to view

--- index.android.js
+++ index.android.js
@@ -4,50 +4,26 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Image,
+    StyleSheet
 } from 'react-native';
 
-export default class AndroidApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+class AndroidApp extends Component {
+    render() {
+        return (
+            <Image source={require('./img_source/extinguisher.png')} style={styles.large_pic}/>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    large_pic: {
+        width: 310,
+        height: 220,
+    }
 });
 
 AppRegistry.registerComponent('AndroidApp', () => AndroidApp);
